592890686f96e990afa986d9fcecccc1b4ea04c2
lnkitson <larowne1@yahoo.com>
2021-10-11 14:37:23 -0400

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+
+
+let colorInput =  document.querySelector("#color");
+let hexInput =  document.querySelector("#hex");
+
+
+colorInput.addEventListener('input', () =>
+{
+	let color =colorInput.value;
+	hexInput.value=color;
+	document.body.style.backgroundColor =color;
+
+});
